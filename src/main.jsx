bdaf37aaadfd63d4ae8f81b6c35e9ddf641d92e5
dhson <saozzang@gmail.com>
2025-11-23 22:59:02 +0900

    [#10]애플리케이션시작지점추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import AppRouter from "./AppRouter";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// root 엘리먼트를 DOM에서 선택 (React 앱이 마운트될 위치)
+const container = document.getElementById("root");
+
+// createRoot를 통해 root 객체 생성 (React 18 방식)
+const root = createRoot(container);
+
+// AppRouter 컴포넌트를 root에 렌더링 (초기 탭 prop는 "home")
+root.render(<AppRouter tab="home" />);
